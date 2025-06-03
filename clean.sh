788238dfb23c4761bee61db0bd4d6b55d86a9c7a
Mohamed <mmohamed@c2-login-001.cm.cluster>
2025-06-03 10:51:35 -0500

    Publish site

--- clean.sh
+++ clean.sh
@@ -1,8 +1,10 @@
-rm -rf /storage2/fs1/dspencer/Active/spencerlab/app/modules/spack
-rm -rf ~/.spack
-#!/bin/bash
-# Clean Quarto cache and generated files
-find docs/ -type d -name "*_cache" -exec rm -rf {} +
-find docs/ -type f -name "*.html.md" -exec rm -f {} +
-rm -rf docs/_site
-rm -rf docs/_freeze
+# rm -rf /storage2/fs1/dspencer/Active/spencerlab/app/modules/spack
+# rm -rf ~/.spack
+# #!/bin/bash
+# # Clean Quarto cache and generated files
+# find docs/ -type d -name "*_cache" -exec rm -rf {} +
+# find docs/ -type f -name "*.html.md" -exec rm -f {} +
+# rm -rf _site
+# rm -rf _freeze
+
+# srun --nodelist=c2-bigmem-002 --time=24:00:00 --ntasks=4 --mem=64G --pty /bin/bash
